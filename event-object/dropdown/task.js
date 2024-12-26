7b8d98d50d10a4722debe4a58a9120035fbfd153
oligo <oligo83@gmail.com>
2024-12-26 22:37:28 +0700

    dropdown commit

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,23 @@
+const dropdownValue = document.querySelector('.dropdown__value');
+const dropdownList = document.querySelector('.dropdown__list');
+
+dropdownValue.onclick = () => {
+    if (dropdownList.style.display === 'block') {
+        dropdownList.style.display = 'none';
+        dropdownList.classList.remove('dropdown__list_active');
+    } else {
+        dropdownList.style.display = 'block';
+        dropdownList.classList.add('dropdown__list_active');
+    }
+}
+
+dropdownList.onclick = (event) => {
+    const clickedItem = event.target;
+    event.preventDefault();
+
+    if (clickedItem.classList.contains('dropdown__link')) {
+        dropdownList.classList.add('dropdown__list_active');
+        dropdownValue.textContent = clickedItem.textContent;
+        dropdownList.style.display = 'none';
+    }
+};
