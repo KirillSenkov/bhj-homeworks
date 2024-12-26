cf02c2fcfc63ed63ed45d261a6da48f8d3ad5a23
oligo <oligo83@gmail.com>
2024-12-26 22:52:39 +0700

    dropdown commit for multipple buttons

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,23 +1,27 @@
-const dropdownValue = document.querySelector('.dropdown__value');
-const dropdownList = document.querySelector('.dropdown__list');
+const dropdowns = document.querySelectorAll('.dropdown');
 
-dropdownValue.onclick = () => {
-    if (dropdownList.style.display === 'block') {
-        dropdownList.style.display = 'none';
-        dropdownList.classList.remove('dropdown__list_active');
-    } else {
-        dropdownList.style.display = 'block';
-        dropdownList.classList.add('dropdown__list_active');
+dropdowns.forEach(dropdown => {
+    const dropdownValue = dropdown.querySelector('.dropdown__value');
+    const dropdownList = dropdown.querySelector('.dropdown__list');
+
+    dropdownValue.onclick = () => {
+        if (dropdownList.style.display === 'block') {
+            dropdownList.style.display = 'none';
+            dropdownList.classList.remove('dropdown__list_active');
+        } else {
+            dropdownList.style.display = 'block';
+            dropdownList.classList.add('dropdown__list_active');
+        }
     }
-}
 
-dropdownList.onclick = (event) => {
-    const clickedItem = event.target;
-    event.preventDefault();
+    dropdownList.onclick = (event) => {
+        const clickedItem = event.target;
+        event.preventDefault();
 
-    if (clickedItem.classList.contains('dropdown__link')) {
-        dropdownList.classList.add('dropdown__list_active');
-        dropdownValue.textContent = clickedItem.textContent;
-        dropdownList.style.display = 'none';
-    }
-};
+        if (clickedItem.classList.contains('dropdown__link')) {
+            dropdownList.classList.add('dropdown__list_active');
+            dropdownValue.textContent = clickedItem.textContent;
+            dropdownList.style.display = 'none';
+        }
+    };
+});
